538f2481bf88c1f46743b0b982680f5aed68ceca
Bhanu Tejasree Kandula <bhanutejasree.kandula@gmail.com>
2026-06-08 11:59:24 +0000

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <header>
+      <h1>Doctor app</h1>
+      <div>
+        <button>Home</button>
+        <button>Add doctor</button>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
