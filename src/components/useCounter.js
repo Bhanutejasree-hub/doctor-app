248399b9ad3712a47c04ff60b3b4b2654e571f07
Kandula Bhanu TejaSree <bhanutejasree.kandula@gmail.com>
2026-06-12 11:40:50 +0530

    Add custom useCounter hook for managing count

---
+++ src/components/useCounter.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+function useCounter() {
+  let [count, setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+  function decrement() {
+    setCount(couunt - 1);
+  }
+  return { count, increment, decrement };
+}
+export default useCounter;
